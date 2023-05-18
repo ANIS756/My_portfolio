c87c12d7c9c40d79110a2b930328ed07826cd266
https://github.com/ANIS756 <anismojumder4@gmail.com>
2023-05-18 21:18:46 +0600

    MY Portfolio Update -2

--- resources/views/website/includes/header.blade.php
+++ resources/views/website/includes/header.blade.php
@@ -10,7 +10,7 @@
 <!-- Navbar Start -->
 
 <nav class="navbar navbar-expand-lg bg-white navbar-light fixed-top shadow py-lg-0 px-4 px-lg-5" data-wow-delay="0.1s">
-    <a href="index.html" class="navbar-brand d-block d-lg-none">
+    <a href="{{route('landing')}}" class="navbar-brand d-block d-lg-none">
         <h1 class="text-primary fw-bold m-0">ProMan</h1>
     </a>
     <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -18,7 +18,7 @@
     </button>
     <div class="collapse navbar-collapse justify-content-between py-4 py-lg-0" id="navbarCollapse">
         <div class="navbar-nav mx-auto py-0">
-            <a href="index.html" class="navbar-brand bg-secondary py-3 px-4 mx-3 d-none d-lg-block">
+            <a href="{{route('landing')}}" class="navbar-brand bg-secondary py-3 px-4 mx-3 d-none d-lg-block">
                 <h1 class="text-primary fw-bold m-0">ProMan</h1>
             </a>
             <a href="#home" class="nav-item nav-link active">Home</a>
